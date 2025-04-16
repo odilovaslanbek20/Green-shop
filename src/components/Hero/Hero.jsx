2e321ea300c915b66023b98a6e3e91ec7bbbcc74
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2025-04-16 21:00:30 +0500

    "green-shop.1"

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,10 +29,10 @@ function Hero() {
 												{card?.body}
 											</h2>
 											<h1 className='max-[1080px]:text-[60px] max-[980px]:text-[50px] max-[1080px]:leading-[60px] max-[980px]:leading-[50px] text-[70px] font-black font-["Inter"] max-[898px]:text-[40px] max-[786px]:text-[30px] max-[898px]:leading-[40px] max-[786px]:leading-[30px] max-[652px]:text-[20px] max-[652px]:leading-[20px] uppercase leading-[70px] mt-[7px] max-[530px]:text-[18px] '>
-												{card?.title}{' '}
+												{card?.title}
 												<span className='text-[rgba(70,163,88,1)]'>
 													{card?.title1}
-												</span>{' '}
+												</span>
 											</h1>
 											<p className='text-[14px] font-normal max-[786px]:text-[12px] font-["Inter"] text-[rgba(114,114,114,1)] mt-[5px] max-w-[560px] max-[550px]:hidden'>
 												{card?.text}
@@ -40,7 +40,7 @@ function Hero() {
 											<p className='text-[12px] min-[550px]:hidden font-normal font-["Inter"] text-[rgba(114,114,114,1)] mt-[5px] max-w-[560px] max-[530px]:text-[11px]'>
 												{card?.text1}
 											</p>
-											<button className='w-[140px] h-[40px] rounded-[6px] bg-[rgba(70,163,88,1)] font-bold text-[16px] font-["Inter] text-[#fff] mt-[44px] max-[980px]:mt-[30px] max-[786px]:w-[110px] max-[786px]:h-[35px] max-[898px]:mt-[20px] max-[786px]:mt-[15px] max-[786px]:text-[14px] max-[652px]:bg-transparent max-[652px]:text-[rgba(70,163,88,1)] max-[652px]:flex max-[652px]:items-center max-[652px]:gap-[1px] max-[652px]:mt-0 max-[530px]:text-[12px]'>
+											<button className='w-[140px] h-[40px] rounded-[6px] bg-[rgba(70,163,88,1)] font-bold text-[16px] font-["Inter] text-[#fff] mt-[44px] max-[980px]:mt-[30px] max-[786px]:w-[110px] max-[786px]:h-[35px] max-[898px]:mt-[20px] max-[786px]:mt-[15px] max-[786px]:text-[14px] max-[652px]:bg-transparent max-[652px]:text-[rgba(70,163,88,1)] max-[652px]:flex max-[652px]:items-center max-[652px]:gap-[1px] max-[652px]:mt-0 max-[530px]:text-[12px] cursor-pointer'>
 												{card?.btnText}
 												<FaArrowRightLong className='text-[rgba(70,163,88,1)] min-[651px]:hidden' />
 											</button>
--- src/components/Hero/main.js
+++ src/components/Hero/main.js
@@ -13,24 +13,24 @@ const cardsData = [
 	{
 		id: 2,
 		body: "Welcome to GreenShop",
-		title: "Let’s Make a Better ",
+		title: "LET'S LIVE IN A BETTER ",
 		title1: "Planet",
 		text: "We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create an unique Urban Jungle. Order your favorite plants!",
 		text1: `We are an online plant shop offering a wide range of cheap and trendy plants.`,
 		images: "tuvak.svg",
 		images1: "tuvak1.svg",
-		btnText: "SHOP NOW",
+		btnText: "LET'S START",
 	},
 	{
 		id: 3,
 		body: "Welcome to GreenShop",
-		title: "Let’s Make a Better ",
+		title: "LET'S OBSERVE A BETTER  ",
 		title1: "Planet",
 		text: "We are an online plant shop offering a wide range of cheap and trendy plants. Use our plants to create an unique Urban Jungle. Order your favorite plants!",
 		text1: `We are an online plant shop offering a wide range of cheap and trendy plants.`,
 		images: "tuvak.svg",
 		images1: "tuvak1.svg",
-		btnText: "SHOP NOW",
+		btnText: "GET CREDITS",
 	}
 ]
 
